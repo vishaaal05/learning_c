099438fe0210b173974fc74999f2be0f29701628
Vishal Kumar Gupta <vishaaal05@gmail.com>
2024-12-03 21:30:36 +0530

    update pointers

--- Pointers.c
+++ Pointers.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 int main()
-{
-int a = 5;
-int *p = &a;
+{   
+    int a = 5;
+    int *p = &a;
+    if (p == NULL)
+    {
+        printf("pointer is null");
+    }else 
+    {
+        printf("Null nahi hai");
+    }
+    
+    
+// int a = 5;
+// int *p = &a;
 // printf("Value of a is %d\n", a);
 // printf("Address of the a variable is %p\n", &a);
 // printf("Address of the a variable is %d\n", &a);
@@ -10,9 +21,9 @@ int *p = &a;
 // printf("Address of the a variable is %p\n", *p);
 // printf("Address of the a variable is %p\n", p);
 // printf("Address of the a variable is %d\n", p);
-printf("Value of a is before changing %d\n", a);
-a = 10;
-printf("Value of a is after changing %d", a);
+// printf("Value of a is before changing %d\n", a);
+// a = 10;
+// printf("Value of a is after changing %d", a);
 
  return 0;
 }
