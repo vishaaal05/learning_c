e7b16a7e48032093ddca89f9c65249d1a96f832a
Vishal Kumar Gupta <vishaaal05@gmail.com>
2024-12-03 20:59:09 +0530

    add pointers

---
+++ Pointers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a = 5;
+int *p = &a;
+// printf("Value of a is %d\n", a);
+// printf("Address of the a variable is %p\n", &a);
+// printf("Address of the a variable is %d\n", &a);
+// printf("Address of the a variable is %d\n", *p);
+// printf("Address of the a variable is %p\n", *p);
+// printf("Address of the a variable is %p\n", p);
+// printf("Address of the a variable is %d\n", p);
+printf("Value of a is before changing %d\n", a);
+a = 10;
+printf("Value of a is after changing %d", a);
+
+ return 0;
+}
