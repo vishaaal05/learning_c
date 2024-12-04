8a10d1c15fc40ae82133d7b6355281b9307f30de
Vishal Kumar Gupta <vishaaal05@gmail.com>
2024-12-04 11:59:03 +0530

    modified gets

--- Gets.c
+++ Gets.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main() {
-    char name[3];
+    char name[5];
     printf("Enter your name: ");
-    gets(name);
+    fgets(name, sizeof(name), stdin);
     // fgets(name, sizeof(name), stdin); // Reads max 9 chars + '\0'
     printf("Hello, %s\n", name);
     return 0;
