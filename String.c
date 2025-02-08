d7ef96f9ce3c83773afcf0abe6f178a91bfff2f6
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-02-08 11:53:23 +0530

    refactor String.c to improve user input handling and output formatting

--- String.c
+++ String.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char str1[20] = "Hello";
-    char str2[20] = " World";
-    char str3[20];
+    char firstName[30];
 
-    // String Concatenation
-    strcat(str1, str2);
-    printf("Concatenated: %s\n", str1);
+    // Ask the user to input some text
+    printf("Enter your full name: \n");
 
-    // String Copy
-    strcpy(str3, str1);
-    printf("Copied: %s\n", str3);
+    // Use fgets instead of scanf
+    fgets(firstName, sizeof(firstName), stdin);
 
-    // String Length
-    printf("Length: %lu\n", strlen(str3));
-
-    // String Compare
-    int result = strcmp("banana", "apple");
-    printf("Compare: %d\n", result);
+    // Output the text
+    printf("Hello %s", firstName);
 
     return 0;
 }
