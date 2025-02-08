5184aaf9c84d8d3d24ffe0a1aa2d7458ee4b321b
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-02-08 14:05:58 +0530

    refactor UnionPractice.c to improve output clarity and display memory addresses of union members

--- UnionPractice.c
+++ UnionPractice.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 
-union Data {
+union Data
+{
     int i;
     float f;
     char str[20];
 };
 
-int main() {
+int main()
+{
     union Data data;
-    
+
     // Storing an integer
     data.i = 10;
-    printf("Data as integer: %d\n", data.i);
+
     printf("%p\n", &data.i);
     // Storing a float (overwrites the integer)
     data.f = 220.5;
-    printf("Data as float: %.2f\n", data.f);
+
     printf("%p\n", &data.f);
-    
+
     // Storing a string (overwrites the float)
     strcpy(data.str, "Hello, World!");
-    printf("Data as string: %s\n", data.str);
+
     printf("%p\n", &data.str);
-    
+
+    printf("Data as integer: %d\n", data.i);
+    printf("Data as float: %.2f\n", data.f);
+    printf("Data as string: %s\n", data.str);
     return 0;
 }
