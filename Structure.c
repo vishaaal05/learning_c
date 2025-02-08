09afee232f89db8fbce21d32662d6c1bdd55dc4c
Vishal Kumar Gupta <vishaaal05@gmail.com>
2025-02-08 14:00:19 +0530

    refactor Structure.c to improve output formatting and display memory addresses of structure members

--- Structure.c
+++ Structure.c
@@ -15,7 +15,11 @@ int main()
     s1.phoneNumber = 119;
     s1.favChar = 'V';
 
-    printf("Age is %d \n phone number is %d \n and fav character is %c ", s1.age , s1.phoneNumber, s1.favChar );
+    printf("Age is %d \n phone number is %d \n and fav character is %c \n", s1.age , s1.phoneNumber, s1.favChar );
+
+    printf("%p\n", &s1.age);
+    printf("%p\n", &s1.phoneNumber);
+    printf("%p\n", &s1.favChar);
 
 
     return 0;
